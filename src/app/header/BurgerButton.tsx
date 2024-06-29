3a65b77b186ec8a6aa649fd9cb75846ebe35aaf1
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2024-06-29 12:39:50 +0200

    style: add adaptive design and burger button

---
+++ src/app/header/BurgerButton.tsx
@@ -0,0 +1,16 @@
+import styles from './BurgerButton.module.css';
+
+type BurgerButtonProps = {
+  isOpen: boolean;
+  toggleMenu: () => void;
+};
+
+export default function BurgerButton({ isOpen, toggleMenu }: BurgerButtonProps) {
+  return (
+    <button onClick={toggleMenu} className={`${styles.burgerButton} ${isOpen ? styles.active : ''}`}>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+    </button>
+  );
+}
