c5fbb4c293e7586f412ec32d1a51ed40ffbf7eb6
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2026-02-06 20:02:06 +0100

    fix: remove code duplication

--- src/data/preloadConfig.ts
+++ src/data/preloadConfig.ts
@@ -1,4 +1,4 @@
-import { albums } from './albums';
+import { albumCoverSrcs, albums } from './albums';
 import { covers } from './covers';
 
 export const preloadConfig = {
@@ -29,31 +29,7 @@ export const preloadConfig = {
     '/fonts/Clawmark-OVo1p.otf',
   ],
 
-  albumCovers: [
-    '/assets/covers/friend-of-mine_2_11zon.webp',
-    '/assets/covers/you_6_11zon.webp',
-    '/assets/covers/unsolved-problem_5_11zon.webp',
-    '/assets/covers/album_9_11zon.webp',
-    '/assets/covers/dont-look-down_8_11zon.webp',
-    '/assets/covers/console_7_11zon.webp',
-    '/assets/covers/the-first-time_4_11zon.webp',
-    '/assets/covers/luminance_1_11zon.webp',
-    '/assets/covers/rondo_3_11zon.webp',
-    '/assets/covers/distant-melodies.webp',
-    '/assets/covers/the-last-puzzle_page-0001-min.webp',
-    '/assets/covers/Jacket-denim.webp',
-    '/assets/covers/awake-and-alive.jpg',
-    '/assets/covers/skyglitch.webp',
-    '/assets/covers/Tropical-Lemonade_11zon.webp',
-    '/assets/covers/the-white-city-s-dance.webp',
-    '/assets/covers/to-simulants-with-love.webp',
-    '/assets/covers/The-Art-of-Rebellion.webp',
-    '/assets/covers/the-remastered-collection.webp',
-    '/assets/covers/Twenty-Eight-Past Four.webp',
-    '/assets/covers/driftwood-kingdom.webp',
-    '/assets/covers/the-torch.webp',
-    '/assets/covers/north-of-me.webp',
-  ],
+  albumCovers: albumCoverSrcs,
 };
 
 export const getCriticalAssets = () => [
@@ -79,13 +55,9 @@ export const getCriticalAlbumCovers = () => {
   return Array.from(critical);
 };
 
-export const getPriorityAlbumAssets = () => [
-  '/assets/covers/friend-of-mine_2_11zon.webp',
-  '/assets/covers/you_6_11zon.webp',
-  '/assets/covers/unsolved-problem_5_11zon.webp',
-  '/assets/covers/album_9_11zon.webp',
-  '/assets/covers/dont-look-down_8_11zon.webp',
-];
+const PRIORITY_ALBUM_COVERS_COUNT = 8;
+
+export const getPriorityAlbumAssets = () => albumCoverSrcs.slice(0, PRIORITY_ALBUM_COVERS_COUNT);
 
 export const getAllAlbumAssets = () => preloadConfig.albumCovers;
 
